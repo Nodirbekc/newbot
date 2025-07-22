e2abaf94d44f8f3b77c60eb5cceaf86d203f430c
Nodirbekc <nodirbekahmedov422@gmail.com>
2025-07-22 17:56:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,6 @@ def index():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://newbot-y3d2.onrender.com" + TOKEN)
+    bot.set_webhook(url=f"https://newbot-y3d2.onrender.com/{TOKEN}")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
