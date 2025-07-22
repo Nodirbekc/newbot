62665a1654036e4981c99b22c1754173a4408856
Nodirbekc <nodirbekahmedov422@gmail.com>
2025-07-22 16:08:39 +0500

    fisrt commit

---
+++ mybot/main.py
@@ -0,0 +1,27 @@
+import os
+import telebot
+from flask import Flask, request
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "hillow hillow.")
+
+@app.route('/' + TOKEN, methods=['POST'])
+def receive_update():
+    json_str = request.stream.read().decode("utf-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+@app.route('/')
+def index():
+    return 'Бот работает.', 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
